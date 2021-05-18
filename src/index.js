2db4fb577fadbc1a7cbdc0200a4cdd1dd61b917e
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-18 11:35:20 +0530

    🚀 add database w/ prisma

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
+    link: (_, args) => links.find((l) => l.id === args.id),
   },
   Mutation: {
     post: (parent, args) => {
@@ -25,6 +26,25 @@ const resolvers = {
         url: args.url,
       };
       links.push(link);
+      return link;
+    },
+    updateLink: (_, args) => {
+      const prevLink = links.find((l) => l.id === args.id);
+      links.filter((l) => l.id !== prevLink.id);
+      const link = {
+        id: prevLink.id,
+        description: args.description || prevLink.description,
+        url: args.url || prevLink.url,
+      };
+      links.push(link);
+
+      return link;
+    },
+
+    deleteLink: (_, args) => {
+      const link = links.find((l) => l.id === args.id);
+      links.filter((l) => l.id !== link.id);
+
       return link;
     },
   },

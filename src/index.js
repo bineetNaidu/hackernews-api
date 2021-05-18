8c814c21ec5b355dd5d3745930bc05dcd477f7dd
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-18 10:39:42 +0530

    🚀 update schema

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
+
 const typeDefs = gql`
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+
   type Query {
     info: String!
+    feed: [Link!]!
   }
 `;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
 

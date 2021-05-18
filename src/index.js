6c525c7e87f0e534a80297b515cd1b4ddfe3530b
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-18 10:45:00 +0530

    ✨ housekeeping

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
 
 let links = [
   {
@@ -8,33 +10,28 @@ let links = [
   },
 ];
 
-const typeDefs = gql`
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-`;
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
   },
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers,
 });
 
